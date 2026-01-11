9f1ac54df874d8c5fb1b528454988b213e6b4a6c
Kohinor Ali <jewelali@hotmail.co.uk>
2026-01-11 20:28:06 +0000

    Fixed CourseList

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -16,7 +16,7 @@ function CourseList() {
     useEffect(() => {
         fetch(`${process.env.REACT_APP_API_BASE_URL}/api/courses/`)
         .then((response) => response.json())
-        .then((data) => setCourses(data.results))
+        .then((data) => setCourses(data))
         .finally(() => setLoading(false));
     }, []);
 
